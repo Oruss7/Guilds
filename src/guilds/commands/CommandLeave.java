f2d7decd465fa03c4e3dc0285a278a97c8b5fd61
Romain <Romain@PC_One>
2014-08-31 16:13:30 +0200

    V.2

--- src/guilds/commands/CommandLeave.java
+++ src/guilds/commands/CommandLeave.java
@@ -1,62 +1,73 @@
 package guilds.commands;
 
+import guilds.Guild;
 import guilds.GuildsBasic;
-import guilds.messages.Console;
-import guilds.messages.Message;
-import guilds.messages.MessageType;
-import guilds.utilities.Settings;
-
+import guilds.Rank;
+import guilds.User;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class CommandLeave {
 
-	private GuildsBasic GuildsBasic;
-	
-	public CommandLeave(CommandSender sender, String[] args, GuildsBasic GuildsBasic) {
-		
-		this.GuildsBasic = GuildsBasic;
-		
-		if (sender instanceof Player) {
-			Player(args, (Player) sender);
-		} else {
-			Console(args);
-		}
-		
-	}
-	
-	private void Player(String[] args, Player p) {
-		
-		if (p.hasPermission("guilds.user.leave")) {
-			if (GuildsBasic.getEnabled(Settings.ENABLE_CHANGE_GUILD)) {
-				
-				if (GuildsBasic.getPlayerGuild(p) != null) {
-					GuildsBasic.getPlayerGuild(p).subtractOnline();
-				}
-				
-				if (GuildsBasic.PlayerGuild.remove(p.getName()) == null){
-                                    GuildsBasic.sendConsole("player "+p.getName()+" not in guild");
-                                }
-                                    
-				GuildsBasic.PlayerRank.remove(p.getName());
-				new Message(MessageType.GUILD_LEAVE, p, GuildsBasic);
-				
-				GuildsBasic.savePlayers();
-				GuildsBasic.loadPlayers();
-				
-			} else {
-				new Message(MessageType.GUILD_CHOSEN, p, GuildsBasic);
-			}
-		} else {
-			new Message(MessageType.NO_PERMISSION, p, GuildsBasic);
-		}
-		
-	}
-	
-	private void Console(String[] args) {
-		
-		new Console(MessageType.CONSOLE_ERROR, GuildsBasic);
-		
-	}
-	
+    private GuildsBasic plugin;
+
+    public CommandLeave(CommandSender sender, String[] args, GuildsBasic guildsBasic) {
+
+        this.plugin = guildsBasic;
+
+        if (sender instanceof Player) {
+            Player(args, (Player) sender);
+        } else {
+            Console(args);
+        }
+    }
+
+    private void Player(String[] args, Player player) {
+
+        if (player.hasPermission("guilds.user.leave")) {
+            if (plugin.getConfig().getBoolean("config.ENABLE_CHANGE_GUILD")) {
+
+                User user = plugin.getUser(player.getUniqueId());
+                if (user == null || !user.haveGuild()) {
+                    player.sendMessage(plugin.getMessage("NOT_IN_GUILD"));
+                    return;
+                }
+
+                Guild guild = plugin.getGuild(user.getGuild());
+
+                guild.removeMember(user);
+                user.setGuild(null);
+                user.setJoined(null);
+                if(user.getRank().equalsIgnoreCase(Rank.LEAD.toString())){
+                    guild.setLead(null);
+                }
+                user.setRank(null);
+                if (user.getInvitation() == null) {
+                    plugin.removePlayer(user);
+                }
+
+                plugin.getConfiguration().savePlayers();
+
+                for (User member : guild.getListMember()) {
+                    if (member.getOfflinePlayer().isOnline()) {
+                        member.getPlayer().sendMessage(plugin.getMessage("GUILD_AS_LEAVE").replaceAll("%player%", player.getDisplayName()));
+                    }
+                }
+
+                player.sendMessage(plugin.getMessage("GUILD_LEAVE").replaceAll("%player", player.getDisplayName()).replaceAll("%guild%", guild.getName()));;
+
+            } else {
+                player.sendMessage(plugin.getMessage("GUILD_CHOSEN"));
+            }
+        } else {
+            player.sendMessage(plugin.getMessage("NO_PERMISSION"));
+        }
+    }
+
+    private void Console(String[] args) {
+
+        plugin.sendConsole(plugin.getMessage("CONSOLE_ERROR"));
+
+    }
+
 }
