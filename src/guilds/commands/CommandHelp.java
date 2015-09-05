3b8d7cbcb3ae2e78cf42338a88f17a194bb4173b
largo-7 <Romain@Neo>
2015-09-05 16:27:13 +0200

    Revert "Help en français"
    This reverts commit 484f2e6f2c3fb2dc1e0ae0dc9c02b95c840370e2.

--- src/guilds/commands/CommandHelp.java
+++ src/guilds/commands/CommandHelp.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 
 public class CommandHelp {
 
-    private final GuildsBasic plugin;
+    private GuildsBasic plugin;
 
     public CommandHelp(CommandSender sender, String[] args, GuildsBasic guildsBasic) {
 
@@ -22,27 +22,23 @@ public class CommandHelp {
 
     private void Player(String[] args, Player player) {
 
+        player.sendMessage(ChatColor.YELLOW + "=============== Guilds v" + plugin.v + " ===============");
+        player.sendMessage(ChatColor.AQUA + "/guilds info [player] " + ChatColor.YELLOW + ": display informations about player.");
+        player.sendMessage(ChatColor.AQUA + "/guilds invite " + ChatColor.YELLOW + ": invite player to join your guild.");
+        player.sendMessage(ChatColor.AQUA + "/guilds accept/deny " + ChatColor.YELLOW + ": if you have a invitation, accept or deny to join.");
+        player.sendMessage(ChatColor.AQUA + "/guilds leave " + ChatColor.YELLOW + ": leave current guild.");
+        player.sendMessage(ChatColor.AQUA + "/base " + ChatColor.YELLOW + ": tp to your guild base.");
+        player.sendMessage(ChatColor.AQUA + "/g " + ChatColor.YELLOW + ": talk on guild chanel.");
+        player.sendMessage(ChatColor.AQUA + "/guilds promote <player> [rank] " + ChatColor.YELLOW + ": promote a member.");
+        player.sendMessage(ChatColor.AQUA + "/guilds demote <player> [rank] " + ChatColor.YELLOW + ": demote a member.");
+        player.sendMessage(ChatColor.AQUA + "/guilds kick <player> "+ ChatColor.YELLOW +": kick player from guild.");
         if (player.hasPermission("guilds.admin.*")) {
-            player.sendMessage(ChatColor.YELLOW + "=============== Guilds v" + plugin.v + " ===============");
-        } else {
-            player.sendMessage(ChatColor.YELLOW + "=============== Guilds ===============");
-        }
-        player.sendMessage(ChatColor.AQUA + "/guilds info [joueur] " + ChatColor.YELLOW + ": affiche les informations sur le joueur.");
-        player.sendMessage(ChatColor.AQUA + "/guilds invite " + ChatColor.YELLOW + ": invite un joueur a rejoindre votre guilde.");
-        player.sendMessage(ChatColor.AQUA + "/guilds accept/deny " + ChatColor.YELLOW + ": si vous avez une invitation, accepte ou refuse.");
-        player.sendMessage(ChatColor.AQUA + "/guilds leave " + ChatColor.YELLOW + ": quitte la guilde actuelle.");
-        player.sendMessage(ChatColor.AQUA + "/base " + ChatColor.YELLOW + ": téléporte à la taverne de guilde.");
-        player.sendMessage(ChatColor.AQUA + "/g " + ChatColor.YELLOW + ": parle sur le chant de guilde.");
-        player.sendMessage(ChatColor.AQUA + "/guilds promote <player> [rank] " + ChatColor.YELLOW + ": grade un joueur de votre guilde.");
-        player.sendMessage(ChatColor.AQUA + "/guilds demote <player> [rank] " + ChatColor.YELLOW + ": dégrade un joueur de votre guilde.");
-        player.sendMessage(ChatColor.AQUA + "/guilds kick <player> " + ChatColor.YELLOW + ": exclu un joueur de votre guilde.");
-        if (player.hasPermission("guilds.admin.*")) {
-            player.sendMessage(ChatColor.AQUA + "/guilds create <guild> " + ChatColor.YELLOW + ": créé une guilde.");
-            player.sendMessage(ChatColor.AQUA + "/guilds remove <guild> " + ChatColor.YELLOW + ": supprime une guilde.");
-            player.sendMessage(ChatColor.AQUA + "/guilds setbase [guild] " + ChatColor.YELLOW + ": définit la taverne de guilde (pour les téléportations).");
-            player.sendMessage(ChatColor.AQUA + "/guilds add <player> [guild]" + ChatColor.YELLOW + ": ajoute un joueur à une guilde (sans invitation).");
-            player.sendMessage(ChatColor.AQUA + "/guilds save" + ChatColor.YELLOW + ": sauvegarde la config et les guildes.");
-            player.sendMessage(ChatColor.AQUA + "/guilds load" + ChatColor.YELLOW + ": recharge la config et les guildes.");
+            player.sendMessage(ChatColor.AQUA + "/guilds create <guild> " + ChatColor.YELLOW + ": create guild.");
+            player.sendMessage(ChatColor.AQUA + "/guilds remove <guild> " + ChatColor.YELLOW + ": remove guild.");
+            player.sendMessage(ChatColor.AQUA + "/guilds setbase [guild] " + ChatColor.YELLOW + ": set guilds base.");
+            player.sendMessage(ChatColor.AQUA + "/guilds add <player> [guild]" + ChatColor.YELLOW + ": add a member (without invitation).");
+            player.sendMessage(ChatColor.AQUA + "/guilds save" + ChatColor.YELLOW + ": save to file.");
+            player.sendMessage(ChatColor.AQUA + "/guilds load" + ChatColor.YELLOW + ": load to file.");
         }
         player.sendMessage(ChatColor.YELLOW + "==========================================");
 
@@ -50,17 +46,19 @@ public class CommandHelp {
 
     private void Console(String[] args) {
 
-        plugin.sendConsole("=============== Guilds v" + plugin.v + " ===============");
-        plugin.sendConsole("/guilds save : save to file.");
-        plugin.sendConsole("/guilds load : load from file.");
-        plugin.sendConsole("/guilds leave : leave current guild.");
-        plugin.sendConsole("/guilds kick <player> <guild> : kick player from guild.");
-        plugin.sendConsole("/guilds add <player> <guild> : add player to guild.");
-        plugin.sendConsole("/guilds promote <player> [rank] : promote a member.");
-        plugin.sendConsole("/guilds demote <player> [rank] : demote a member.");
-        plugin.sendConsole("/guilds create <guild> : create guild.");
-        plugin.sendConsole("/guilds remove <guild> : remove guild.");
-        plugin.sendConsole("========================================================");
+        plugin.sendConsole( "=============== Guilds v" + plugin.v + " ===============");
+        plugin.sendConsole( "/guilds save : save to file.");
+        plugin.sendConsole( "/guilds load : load from file.");
+        plugin.sendConsole( "/guilds leave : leave current guild.");
+        plugin.sendConsole( "/guilds kick <player> <guild> : kick player from guild.");
+        plugin.sendConsole( "/guilds add <player> <guild> : add player to guild.");
+        plugin.sendConsole( "/guilds promote <player> [rank] : promote a member.");
+        plugin.sendConsole( "/guilds demote <player> [rank] : demote a member.");
+        plugin.sendConsole( "/guilds create <guild> : create guild.");
+        plugin.sendConsole( "/guilds remove <guild> : remove guild.");
+        plugin.sendConsole( "/guilds setbase <guild> : set guilds base.");
+        plugin.sendConsole( "/base : tp to your guild base.");
+        plugin.sendConsole( "========================================================");
 
     }
 
